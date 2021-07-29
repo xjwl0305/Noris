052c76945d9850e102e220a25a04476f2015cc8a
xjwl0305@gmail.com <yoon8863!>
2021-07-29 14:33:47 +0900

    checkID

--- src/main/java/com/android/restapi/controller/MemberController.java
+++ src/main/java/com/android/restapi/controller/MemberController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "X-AUTH-TOKEN")
 @RequestMapping("/NorisAPI")
 @RestController
 public class MemberController {
