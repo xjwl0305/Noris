36392c45dd536e84bab7c70154c54cdb5a7eccc4
xjwl0305@gmail.com <yoon8863!>
2021-07-29 14:42:33 +0900

    checkID

---
+++ src/main/java/com/android/restapi/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.android.restapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("X-AUTH-TOKEN","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+                .exposedHeaders("Content-Disposition", "X-AUTH-TOKEN", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
