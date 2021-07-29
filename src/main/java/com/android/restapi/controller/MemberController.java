3fb35b5dc595bb2c3fb0475f413e57e94867cd23
xjwl0305@gmail.com <yoon8863!>
2021-07-29 14:13:46 +0900

    checkID

--- src/main/java/com/android/restapi/controller/MemberController.java
+++ src/main/java/com/android/restapi/controller/MemberController.java
@@ -15,8 +15,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3333"})
 @RequestMapping("/NorisAPI")
-//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class MemberController {
 
@@ -24,23 +24,22 @@ public class MemberController {
     public MemberRepository memberRepository;
 
 
-//    @RequestMapping("/login")
-//    public login_response login(@RequestParam(name="id") String ID, @RequestParam(name="password") String Password){
-//        List<user> login = memberRepository.findUser(ID, Password);
-//        login_response response = new login_response();
-//        if(login.size() > 0) {
-//            response.setStatus("success");
-//            response.setName(login.get(0).getName());
-//            response.setEmail(login.get(0).getEmail());
-//            response.setPhone(login.get(0).getPhone());
-//            response.setCompany(login.get(0).getCompany());
-//            return response;
-//        }else{
-//            response.setStatus("failed");
-//            return response;
-//        }
-//    }
-    @CrossOrigin
+    @RequestMapping("/login")
+    public login_response login(@RequestParam(name="id") String ID, @RequestParam(name="password") String Password){
+        List<user> login = memberRepository.findUser(ID, Password);
+        login_response response = new login_response();
+        if(login.size() > 0) {
+            response.setStatus("success");
+            response.setName(login.get(0).getName());
+            response.setEmail(login.get(0).getEmail());
+            response.setPhone(login.get(0).getPhone());
+            response.setCompany(login.get(0).getCompany());
+            return response;
+        }else{
+            response.setStatus("failed");
+            return response;
+        }
+    }
     @RequestMapping("/checkID")
     public response checkID(@RequestParam(name="id") String ID){
         List<user> find = memberRepository.checkID(ID);
