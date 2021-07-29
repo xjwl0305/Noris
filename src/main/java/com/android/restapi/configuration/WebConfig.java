13c2caf1f83ace0011d8185d82cff69a25df6914
xjwl0305@gmail.com <yoon8863!>
2021-07-29 15:42:02 +0900

    checkID

--- src/main/java/com/android/restapi/configuration/WebConfig.java
+++ src/main/java/com/android/restapi/configuration/WebConfig.java
@@ -1,18 +1,19 @@
 package com.android.restapi.configuration;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.android.restapi.configuration.CORSFilter;
+import javax.servlet.FilterRegistration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("X-AUTH-TOKEN","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
-                .exposedHeaders("Content-Disposition", "X-AUTH-TOKEN", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(false).maxAge(3600);
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+        registrationBean.addUrlPatterns("*");
+        return registrationBean;
     }
 }
